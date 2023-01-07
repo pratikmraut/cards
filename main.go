96f7ef43959d4bf09db91fbffd369b8560912e4f
pratik <pratikmraut123@gmail.com>
2023-01-07 19:23:46 +0530

    start

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+	// var card string = "Ace of Spades"
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+	// cards.print()
+	// cards.suffle()
+	// cards.print()
+
+}
